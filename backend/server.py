4b88fdc3608f1aa261fa52c7834c0b352a852d61
Antoine Delègue <delegue.antoine@gmail.com>
2024-04-12 13:12:44 +0200

    :tada: finished the GUI

---
+++ backend/server.py
@@ -0,0 +1,86 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydruid.db import connect
+
+conn = connect(
+    host="druid.k8s.sysware.fr",
+    port=443,
+    path="/druid/v2/sql/",
+    scheme="https",
+    user="user",
+    password="BSrLoanZurN4a8a9",
+)
+curs = conn.cursor()
+
+
+def get_sql_request(
+    OnD="PAR-LIS",
+    trip_type="RT",
+    search_date_min="2021-11-17",
+    search_date_max="2023-11-17",
+):
+    # SQL injectable.
+    return f"""
+    SELECT
+        APPROX_QUANTILE_DS(price, 0.5),
+        airline,
+        advance_purchase
+    FROM (
+        SELECT
+            MIN(db_recos.price_EUR) AS price,
+            db_recos.main_marketing_airline AS airline,
+            s.advance_purchase
+        FROM db_recos
+        JOIN (
+            SELECT
+                search_id,
+                advance_purchase
+            FROM db_searches
+            WHERE db_searches.OnD LIKE '{OnD}' AND db_searches.trip_type LIKE '{trip_type}' AND db_searches.search_date >= '{search_date_min}' AND db_searches.search_date <= '{search_date_max}'
+        ) AS s ON s.search_id = db_recos.search_id
+        GROUP BY db_recos.search_id, db_recos.main_marketing_airline, s.advance_purchase
+    ) AS t
+    GROUP BY airline, advance_purchase
+    """
+
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root(OnD: str, trip_type: str, search_date_min: str, search_date_max: str):
+    request = get_sql_request(OnD, trip_type, search_date_min, search_date_max)
+
+    curs.execute(request)
+    result = curs.fetchall()
+    print(result)
+    return [
+        {
+            "airline": row.airline,
+            "advance_purchase": row.advance_purchase,
+            "price": row._0,
+        }
+        for row in result
+    ]
+
+
+# We believe in security.
+origins = [
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# )
+# for row in curs:
+#    print(row)
+
+if __name__ == "__main__":
+    uvicorn.run(app)
--- gui/src/App.tsx
+++ gui/src/App.tsx
@@ -11,7 +11,8 @@ import { Autocomplete } from "@mui/material";
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFnsV3";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker/DatePicker";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider/LocalizationProvider";
-import React, { useMemo } from "react";
+import * as datefns from "date-fns";
+import React, { useCallback, useState } from "react";
 import {
   VictoryAxis,
   VictoryChart,
@@ -22,26 +23,94 @@ import {
 import "./App.css";
 import { airports } from "./airports";
 
-function App() {
-  const data = useMemo(() => {
-    return {
-      labels: ["January", "February", "March", "April", "May", "June", "July"],
-      datasets: [
-        {
-          label: "Revenue",
-          data: [65, 59, 80, 81, 56, 55, 40],
-          fill: false,
-          borderColor: "rgb(75, 192, 192)",
-          tension: 0.1,
-        },
-      ],
-    };
-  }, []);
+type DataFetchParams = {
+  OnD: string;
+  trip_type: string;
+  search_date_min: string;
+  search_date_max: string;
+};
 
-  const handleChange = () => {
-    console.log("change");
-  };
-  const age = 10;
+const fetchData = async ({
+  OnD,
+  trip_type,
+  search_date_max,
+  search_date_min,
+}: DataFetchParams) => {
+  try {
+    let request = await fetch(
+      `http://localhost:8000/?OnD=${OnD}&trip_type=${trip_type}&search_date_min=${search_date_min}&search_date_max=${search_date_max}`
+    );
+
+    return await request.json();
+  } catch (err) {
+    console.log("Failed to fetch: ", err);
+  }
+};
+
+const randomColor = () => {
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+};
+
+type DataState = {
+  airline: string;
+  lineColor: string;
+  prices: { advance_purchase: number; price: number }[];
+}[];
+
+function App() {
+  const [data, setData] = useState<DataState>([]);
+  const [tripType, setTripType] = useState("RT");
+  const [whereFrom, setWhereFrom] = useState("PAR");
+  const [whereTo, setWhereTo] = useState("LIS");
+
+  const [searchDateMax, setSearchDateMax] = useState<Date>(
+    datefns.parse("2025/11/17", "yyyy/MM/dd", new Date())
+  );
+  const [searchDateMin, setSearchDateMin] = useState<Date>(
+    datefns.parse("2020/11/17", "yyyy/MM/dd", new Date())
+  );
+
+  const makeSearch = useCallback(async () => {
+    const OnD = `${whereFrom}-${whereTo}`;
+    const searchDateMaxStr = datefns.format(searchDateMax, "yyyy/MM/dd");
+    const searchDateMinStr = datefns.format(searchDateMin, "yyyy/MM/dd");
+
+    let data = await fetchData({
+      OnD: OnD,
+      search_date_max: searchDateMaxStr,
+      search_date_min: searchDateMinStr,
+      trip_type: tripType,
+    });
+    let airlines: string[] = [];
+    for (let dataPoint of data) {
+      if (airlines.includes(dataPoint.airline)) {
+        continue;
+      }
+      airlines.push(dataPoint.airline);
+    }
+
+    console.log(
+      "fetch parameters: ",
+      OnD,
+      searchDateMaxStr,
+      searchDateMinStr,
+      tripType
+    );
+    console.log(data);
+
+    let result: DataState = [];
+    for (let airline of airlines) {
+      let prices = data
+        .filter((d) => d.airline === airline)
+        .map((d) => ({
+          advance_purchase: d.advance_purchase,
+          price: d.price,
+        }));
+      result.push({ airline, prices, lineColor: randomColor() });
+    }
+
+    setData(result);
+  }, [setData, tripType, whereFrom, whereTo, searchDateMax, searchDateMin]);
 
   return (
     <div className="flex flex-col items-center">
@@ -52,17 +121,19 @@ function App() {
         </h2>
         <div className="h-4" />
         <div className="flex justify-center">
-          <FormControl className="w-[100px]">
+          <FormControl className="w-[200px]">
             <InputLabel>Trip type</InputLabel>
             <Select
               labelId="demo-simple-select-label"
               id="demo-simple-select"
-              value={age}
-              label="Age"
-              onChange={handleChange}
+              value={tripType}
+              label="Trip type"
+              onChange={(evt) => {
+                setTripType(evt.target.value === "RT" ? "RT" : "OW");
+              }}
             >
-              <MenuItem value={10}>One way</MenuItem>
-              <MenuItem value={20}>Round trip</MenuItem>
+              <MenuItem value={"OW"}>One way</MenuItem>
+              <MenuItem value={"RT"}>Round trip</MenuItem>
             </Select>
           </FormControl>
           <div className="w-4 inline-block" />
@@ -70,8 +141,16 @@ function App() {
             className="inline"
             options={airports}
             sx={{ width: 300 }}
+            value={whereFrom}
             renderInput={(params) => (
-              <TextField {...params} variant="outlined" label="Where from?" />
+              <TextField
+                {...params}
+                variant="outlined"
+                onChange={(evt) => {
+                  setWhereFrom(evt.target.value);
+                }}
+                label="Where from?"
+              />
             )}
           />
           <div className="w-4 inline-block" />
@@ -79,22 +158,40 @@ function App() {
           <Autocomplete
             options={airports}
             sx={{ width: 300 }}
+            value={whereTo}
             renderInput={(params) => (
-              <TextField {...params} variant="outlined" label="Where to?" />
+              <TextField
+                {...params}
+                variant="outlined"
+                onChange={(evt) => {
+                  setWhereTo(evt.target.value);
+                }}
+                label="Where to?"
+              />
             )}
           />
           <div className="w-4 inline-block" />
 
           <LocalizationProvider dateAdapter={AdapterDateFns}>
-            <DatePicker label="Search for trips after " />
+            <DatePicker
+              value={searchDateMin}
+              onChange={(newValue) => {
+                setSearchDateMin(newValue ?? new Date());
+              }}
+              label="Search for trips after "
+            />
             <div className="w-4 inline-block" />
 
-            <DatePicker label="Search for trips before" />
+            <DatePicker
+              value={searchDateMax}
+              onChange={(newValue) => setSearchDateMax(newValue ?? new Date())}
+              label="Search for trips before"
+            />
           </LocalizationProvider>
         </div>
         <div className="h-4" />
         <div className="text-left">
-          <Button variant="contained" color="primary">
+          <Button onClick={makeSearch} variant="contained" color="primary">
             Explore
           </Button>
         </div>
@@ -112,25 +209,26 @@ function App() {
             orientation="horizontal"
             gutter={20}
             style={{ border: { stroke: "black" }, title: { fontSize: 20 } }}
-            data={[
-              { name: "Airfrance", symbol: { fill: "tomato" } },
-              { name: "Easy-Jet", symbol: { fill: "orange" } },
-              { name: "Delta", symbol: { fill: "gold" } },
-            ]}
+            data={data.map((d) => ({
+              name: d.airline,
+              symbol: { fill: d.lineColor },
+            }))}
           />
-          <VictoryLine
-            style={{
-              data: { stroke: "#c43a31" },
-              parent: { border: "1px solid #ccc" },
-            }}
-            data={[
-              { x: 1, y: 2 },
-              { x: 2, y: 3 },
-              { x: 3, y: 5 },
-              { x: 4, y: 4 },
-              { x: 5, y: 7 },
-            ]}
-          />
+          {data.map((d) => {
+            return (
+              <VictoryLine
+                key={d.airline}
+                style={{
+                  data: { stroke: d.lineColor },
+                  parent: { border: "1px solid #ccc" },
+                }}
+                data={d.prices.map((p) => ({
+                  x: p.advance_purchase,
+                  y: p.price,
+                }))}
+              />
+            );
+          })}
           <VictoryAxis
             label="Advance purchase (in days)"
             style={{
