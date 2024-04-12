38348bf4841f8470c00758e665f1257a1b9dc105
Antoine Delègue <delegue.antoine@gmail.com>
2024-04-12 09:48:25 +0200

    :tada: start of GUI

---
+++ gui/README.md
@@ -0,0 +1,3 @@
+# GUI for Amadeus
+
+Made using React, TypeScript and Vite.
---
+++ gui/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-red-600">Airline dashboard</h1>
+      <p>Hello, world!</p>
+    </div>
+  );
+}
+
+export default App;
